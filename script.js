e963d3b961b5cd8e1f9f9ebf676f37a56f9bc8b3
yovelsapir <yovelsapir@gmail.com>
2019-12-22 15:13:47 +0200

    finish buttons functionality

--- script.js
+++ script.js
@@ -14,40 +14,167 @@ const CONTACT_GROUP = 'contact-group';
 document.querySelectorAll('.message-block').forEach((el) => el.style.top = "0px");
 document.querySelectorAll('.group-container').forEach((el, i) => el.style.order = i);
 
+let steps = 0;
+// let isRight = true;
+
+const buttonSteps = {
+    'show-me-your-work': {
+        'what-you-can-do': {
+            'show-me-smooth-animation': {
+                'contact-info': true
+            },
+        },
+        'show-me-smooth-animation': {
+            'what-you-can-do': {
+                'contact-info': true
+            },
+        }
+    }
+}
+
 document.querySelector(`#${HOME_GROUP} .show-me-your-work-btn`).addEventListener('click', () => {
-    document.getElementById(WORK_GROUP).classList.remove('hidden');
+    showPage(WORK_GROUP, 1);
+    document.querySelector(`#${HOME_GROUP} .what-you-can-do-btn`).remove();
     createGallery('.gallery-block-show-me-your-work');
+
+
+    const whatYouCanDoButton = createButton("What you can do?", '#what-you-can-do-group', WORK_GROUP);
+    const smoothAnimationButton = createButton("Show me smooth animations", '#smooth-animation-group', WORK_GROUP);
+
+    whatYouCanDoButton.addEventListener('click', () => {
+        removeButton(smoothAnimationButton);
+        showPage(WHAT_YOU_CAN_DO_GROUP, 2);
+        const w_smoothAnimationButton = createButton("Show me smooth animations", '#smooth-animation-group', WHAT_YOU_CAN_DO_GROUP);
+        w_smoothAnimationButton.addEventListener('click', () => {
+            showPage(SMOOTH_ANIMATION_GROUP, 3);
+            createGallery('.gallery-block-smooth-animation');
+            const w_contactInfoButton = createButton("Contact info", '#contact-group', SMOOTH_ANIMATION_GROUP);
+            w_contactInfoButton.addEventListener('click', () => {
+                showPage(CONTACT_GROUP, 4);
+            });
+        });
+    });
+
+    smoothAnimationButton.addEventListener('click', () => {
+        removeButton(whatYouCanDoButton);
+        showPage(SMOOTH_ANIMATION_GROUP, 2);
+        createGallery('.gallery-block-smooth-animation');
+
+        const s_whatYouCanDoButton = createButton("What you can do?", '#what-you-can-do-group', SMOOTH_ANIMATION_GROUP);
+        s_whatYouCanDoButton.addEventListener('click', () => {
+            showPage(WHAT_YOU_CAN_DO_GROUP, 3);
+
+            const s_contactInfoButton = createButton("Contact info", '#contact-group', WHAT_YOU_CAN_DO_GROUP);
+            s_contactInfoButton.addEventListener('click', () => {
+                showPage(CONTACT_GROUP, 4);
+            });
+        });
+
+    });
 });
 
 document.querySelector(`#${HOME_GROUP} .what-you-can-do-btn`).addEventListener('click', () => {
-    document.getElementById(WHAT_YOU_CAN_DO_GROUP).classList.remove('hidden');
+    showPage(WHAT_YOU_CAN_DO_GROUP, 1);
+    document.querySelector(`#${HOME_GROUP} .show-me-your-work-btn`).remove();
+    
+    const showMeYourWorkButton = createButton("Show me Your work", '#show-me-your-work-group', WHAT_YOU_CAN_DO_GROUP);
+    const smoothAnimationButton = createButton("Show me smooth animations", '#smooth-animation-group', WHAT_YOU_CAN_DO_GROUP);
+
+    showMeYourWorkButton.addEventListener('click', () => {
+        removeButton(smoothAnimationButton);
+        showPage(WORK_GROUP, 2);
+        createGallery('.gallery-block-show-me-your-work');
+
+        const w_smoothAnimationButton = createButton("Show me smooth animations", '#smooth-animation-group', WORK_GROUP);
+        w_smoothAnimationButton.addEventListener('click', () => {
+            showPage(SMOOTH_ANIMATION_GROUP, 3);
+            createGallery('.gallery-block-smooth-animation');
+            const w_contactInfoButton = createButton("Contact info", '#contact-group', SMOOTH_ANIMATION_GROUP);
+            w_contactInfoButton.addEventListener('click', () => {
+                showPage(CONTACT_GROUP, 4);
+            });
+        });
+
+    });
+
+    smoothAnimationButton.addEventListener('click', () => {
+        removeButton(showMeYourWorkButton);
+        showPage(SMOOTH_ANIMATION_GROUP, 2);
+        createGallery('.gallery-block-smooth-animation');
+
+        const s_showMeYourWorkButton = createButton("Show me Your work", '#show-me-your-work-group', SMOOTH_ANIMATION_GROUP);
+        s_showMeYourWorkButton.addEventListener('click', () => {
+            showPage(WORK_GROUP, 3);
+            createGallery('.gallery-block-show-me-your-work');
+
+            const w_contactInfoButton = createButton("Contact info", '#contact-group', WORK_GROUP);
+            w_contactInfoButton.addEventListener('click', () => {
+                showPage(CONTACT_GROUP, 4);
+            });
+        });
+    });
+
 });
 
-document.querySelector(`#${WORK_GROUP} .what-you-can-do-btn`).addEventListener('click', () => {
-    document.getElementById(WHAT_YOU_CAN_DO_GROUP).classList.remove('hidden');
-});
+function showPage(group, order) {
+    document.getElementById(group).style.order = order;
+    document.getElementById(group).classList.remove('hidden');
+}
 
-document.querySelector(`#${WORK_GROUP} .smooth-animation-btn`).addEventListener('click', () => {
-    document.getElementById(SMOOTH_ANIMATION_GROUP).classList.remove('hidden');
-    createGallery('.gallery-block-smooth-animation');
-});
+function createButton(text, link, group) {
+    const button = document.createElement('a');
+    var t = document.createTextNode(text);
+    button.appendChild(t);
+    button.setAttribute('href', link);
+    button.classList.add('btn');
+    document.querySelector(`#${group} .button-group`).appendChild(button);
+    return button;
+}
 
-document.querySelector(`#${WHAT_YOU_CAN_DO_GROUP} .show-me-your-work-btn`).addEventListener('click', () => {
-    document.getElementById(WORK_GROUP).classList.remove('hidden');
-    createGallery('.gallery-block-show-me-your-work');
-});
+function removeButton(el) {
+    el.remove();
+    // el.removeEventListener('click');
+}
+// document.querySelector(`#${WHAT_YOU_CAN_DO_GROUP} .show-me-your-work-btn`).addEventListener('click', () => {
+//     if (steps == 1) {
+//         document.getElementById(WORK_GROUP).classList.remove('hidden');
+//         createGallery('.gallery-block-show-me-your-work');
+//         steps++;
+//     }
+// });
 
-document.querySelector(`#${WHAT_YOU_CAN_DO_GROUP} .contact-btn`).addEventListener('click', () => {
-    document.getElementById(CONTACT_GROUP).classList.remove('hidden');
-});
+// document.querySelector(`#${WHAT_YOU_CAN_DO_GROUP} .smooth-animation-btn`).addEventListener('click', () => {
+//     if (steps == 2) {
+//         document.getElementById(SMOOTH_ANIMATION_GROUP).classList.remove('hidden');
+//         document.querySelector(`#${WHAT_YOU_CAN_DO_GROUP} .show-me-your-work-btn`).remove();
+//         createGallery('.gallery-block-smooth-animation');
+//         steps++;
+//     }
+// });
 
-document.querySelector(`#${SMOOTH_ANIMATION_GROUP} .what-you-can-do-btn`).addEventListener('click', () => {
-    document.getElementById(WHAT_YOU_CAN_DO_GROUP).classList.remove('hidden');
-});
+// document.querySelector(`#${WORK_GROUP} .what-you-can-do-btn`).addEventListener('click', () => {
+//     if (steps == 1) {
+//         document.getElementById(WHAT_YOU_CAN_DO_GROUP).classList.remove('hidden');
+//         document.querySelector(`#${WORK_GROUP} .smooth-animation-btn`).remove();
+//         steps++;
+//     }
+// });
 
-document.querySelector(`#${SMOOTH_ANIMATION_GROUP} .contact-btn`).addEventListener('click', () => {
-    document.getElementById(CONTACT_GROUP).classList.remove('hidden');
-});
+// document.querySelector(`#${WORK_GROUP} .smooth-animation-btn`).addEventListener('click', () => {
+//     document.getElementById(SMOOTH_ANIMATION_GROUP).classList.remove('hidden');
+//     createGallery('.gallery-block-smooth-animation');
+// });
+
+// document.querySelector(`#${SMOOTH_ANIMATION_GROUP} .what-you-can-do-btn`).addEventListener('click', () => {
+//     document.getElementById(WHAT_YOU_CAN_DO_GROUP).classList.remove('hidden');
+// });
+
+// document.querySelector(`#${SMOOTH_ANIMATION_GROUP} .contact-btn`).addEventListener('click', (e) => {
+//     if (steps == 3) {
+//         document.getElementById(CONTACT_GROUP).classList.remove('hidden');
+//         document.querySelector(`#${SMOOTH_ANIMATION_GROUP} .what-you-can-do-btn`).remove();
+//     }
+// });
 
 createGallery('.gallery-block-home-group');
 
@@ -85,28 +212,28 @@ emergence.init({
 });
 
 function createGallery(selector, horizontalAlign) {
-    const galleryBlockHomeGroup = document.querySelector(selector);
-    if(!galleryBlockHomeGroup.classList.contains('gallery-active')){
+    const galleryBlock = document.querySelector(selector);
+    if (!galleryBlock.classList.contains('gallery-active')) {
         const _horizontalAlign = horizontalAlign == 'right' ? horizontalAlign : 'left';
-        galleryBlockHomeGroup.classList.add('gallery-active');
-        galleryBlockHomeGroup.style.position = 'relative';
-        const galleryBlockHomeGroupChildrenCount = galleryBlockHomeGroup.childElementCount;
-        const galleryBlockHomeGroupChildrenClientWidth = galleryBlockHomeGroup.clientWidth;
-        const galleryBlockHomeGroupChildrenOffset = (galleryBlockHomeGroupChildrenCount * (galleryBlockHomeGroupChildrenClientWidth / galleryBlockHomeGroupChildrenCount)) / 1.5;
-        galleryBlockHomeGroup.style[_horizontalAlign] = -galleryBlockHomeGroupChildrenOffset + 'px';
-        let galleryBlockHomeGroupStep = 0;
-    
-        galleryBlockHomeGroup.addEventListener('click', () => {
-            const galleryBlockHomeGroupPos = parseInt(galleryBlockHomeGroup.style[_horizontalAlign].replace('px', ''));
-            galleryBlockHomeGroupStep++;
-            if (galleryBlockHomeGroupStep >= galleryBlockHomeGroupChildrenCount) {
-                galleryBlockHomeGroup.style[_horizontalAlign] = -galleryBlockHomeGroupChildrenOffset + 'px'
-                galleryBlockHomeGroupStep = 0;
+        galleryBlock.classList.add('gallery-active');
+        galleryBlock.style.position = 'relative';
+        const galleryBlockChildrenCount = galleryBlock.childElementCount;
+        const galleryBlockChildrenClientWidth = galleryBlock.clientWidth;
+        const galleryBlockChildrenOffset = (galleryBlockChildrenCount * (galleryBlockChildrenClientWidth / galleryBlockChildrenCount)) / 1.5;
+        galleryBlock.style[_horizontalAlign] = -galleryBlockChildrenOffset + 'px';
+        let galleryBlockStep = 0;
+
+        galleryBlock.addEventListener('click', () => {
+            const galleryBlockPos = parseInt(galleryBlock.style[_horizontalAlign].replace('px', ''));
+            galleryBlockStep++;
+            if (galleryBlockStep >= galleryBlockChildrenCount) {
+                galleryBlock.style[_horizontalAlign] = -galleryBlockChildrenOffset + 'px'
+                galleryBlockStep = 0;
             } else {
-                if (galleryBlockHomeGroupStep == galleryBlockHomeGroupChildrenCount - 1) {
-                    galleryBlockHomeGroup.style[_horizontalAlign] = '0px';
+                if (galleryBlockStep == galleryBlockChildrenCount - 1) {
+                    galleryBlock.style[_horizontalAlign] = '0px';
                 } else {
-                    galleryBlockHomeGroup.style[_horizontalAlign] = (galleryBlockHomeGroupPos) + (galleryBlockHomeGroupChildrenClientWidth / galleryBlockHomeGroupChildrenCount) + 'px';
+                    galleryBlock.style[_horizontalAlign] = (galleryBlockPos) + (galleryBlockChildrenClientWidth / galleryBlockChildrenCount) + 'px';
                 }
             }
         });
