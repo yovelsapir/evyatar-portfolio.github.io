0b594d59b4a1dc853e396c1793eb785d2c00fc8d
yovelsapir <yovelsapir@gmail.com>
2020-01-26 23:49:00 +0200

    .

--- dist/webflow.f4258b2d.js
+++ dist/webflow.f4258b2d.js
@@ -11163,7 +11163,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49508" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50087" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
