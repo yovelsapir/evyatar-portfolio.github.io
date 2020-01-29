c0565a61830fae698369d1ce99d5b5dad9f2678b
Evyatar Mor <evyatar@macs-MacBook-Pro.local>
2020-01-30 00:30:51 +0200

    test

--- dist/webflow.f4258b2d.js
+++ dist/webflow.f4258b2d.js
@@ -11163,7 +11163,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61845" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50702" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
