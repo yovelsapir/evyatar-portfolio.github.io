8e5db6c80ef7f1bfe1b422379145d369244fea7a
yovelsapir <yovelsapir@gmail.com>
2020-01-29 17:34:21 +0200

    .

--- dist/normalize.1114379f.js
+++ dist/normalize.1114379f.js
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62140" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62234" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
